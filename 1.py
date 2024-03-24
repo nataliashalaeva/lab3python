eee2a4b4c7d8a2ba691e5e5343a9d9354dd01881
nataliashalaeva <112845670+nataliashalaeva@users.noreply.github.com>
2024-03-24 18:33:33 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -23,8 +23,19 @@ class Quad:
     def compare(self, other):
         # Сравниваем два четырехугольника по площади
         return self.area() - other.area()
-        
-def point_inside_polygon(self, point, polygon):
+
+    def is_intersect(self, other):
+        # Проверяем пересечение двух четырехугольников
+        # Для простоты, будем считать, что они пересекаются, если хотя бы одна вершина одного четырехугольника находится внутри другого
+        for point in self.points:
+            if self.point_inside_polygon(point, other.points):
+                return True
+        for point in other.points:
+            if self.point_inside_polygon(point, self.points):
+                return True
+        return False
+
+    def point_inside_polygon(self, point, polygon):
         # Функция для определения, находится ли точка внутри полигона
         n = len(polygon)
         inside = False
@@ -41,17 +52,50 @@ def point_inside_polygon(self, point, polygon):
             p1x, p1y = p2x, p2y
         return inside
 
- def is_intersect(self, other):
-        # Проверяем пересечение двух четырехугольников
-        # Для простоты, будем считать, что они пересекаются, если хотя бы одна вершина одного четырехугольника находится внутри другого
+class Pentagon:
+    def __init__(self, points):
+        self.points = points
+
+    def area(self):
+        # Площадь пятиугольника можно вычислить как сумму площадей треугольников, образованных его сторонами и центром
+        a = self.points[0]
+        b = self.points[1]
+        c = self.points[2]
+        d = self.points[3]
+        e = self.points[4]
+        # Площадь треугольника можно найти по формуле Герона
+        def triangle_area(p1, p2, p3):
+            s = (math.dist((p1.x, p1.y), (p2.x, p2.y)) + math.dist((p2.x, p2.y), (p3.x, p3.y)) + math.dist((p1.x, p1.y), (p3.x, p3.y))) / 2
+            return math.sqrt(s * (s - math.dist((p1.x, p1.y), (p2.x, p2.y))) * (s - math.dist((p2.x, p2.y), (p3.x, p3.y))) * (s - math.dist((p1.x, p1.y), (p3.x, p3.y))))
+        return triangle_area(a, b, c) + triangle_area(c, d, e)
+
+    def compare(self, other):
+        # Сравниваем два пятиугольника по площади
+        return self.area() - other.area()
+
+    def is_intersect(self, other):
+        # Проверяем пересечение двух пятиугольников (аналогично четырехугольникам)
         for point in self.points:
             if self.point_inside_polygon(point, other.points):
                 return True
         for point in other.points:
-            if self.point_inside_polygon(point, self.points):
+           if self.point_inside_polygon(point, self.points):
                 return True
         return False
 
-class Pentagon:
-    def __init__(self, points):
-        self.points = points
+    def point_inside_polygon(self, point, polygon):
+        # Функция для определения, находится ли точка внутри полигона (аналогично для четырехугольников)
+        n = len(polygon)
+        inside = False
+        p1x, p1y = polygon[0].x, polygon[0].y
+        for i in range(n + 1):
+            p2x, p2y = polygon[i % n].x, polygon[i % n].y
+            if point.y > min(p1y, p2y):
+                if point.y <= max(p1y, p2y):
+                    if point.x <= max(p1x, p2x):
+                        if p1y != p2y:
+                            xinters = (point.y - p1y) * (p2x - p1x) / (p2y - p1y) + p1x
+                        if p1x == p2x or point.x <= xinters:
+                            inside = not inside
+            p1x, p1y = p2x, p2y
+        return inside
